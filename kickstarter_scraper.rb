51f388d20949984a7cccea8ed16b975523a8b561
Andre King <andreking1031@gmail.com>
2020-09-02 23:58:12 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,9 +10,11 @@ require "pry"
 create_project_hash
 
 def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
- 
-  projects = {}
+ projects = {}
   
+  kickstarter.css("li.project.grid_4").each do |project|
+    
+  end
   
+  projects
+end
